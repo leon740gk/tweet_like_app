ec332ca6cb4c3f4c4f51752842efcb77cdf6e4e6
leon740gk <leon740gk@gmail.com>
2021-01-01 13:17:39 +0200

    Add JS to home page with list of tweets

--- tweets/views.py
+++ tweets/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
 
 
 def home_page(request, *args, **kwargs):
-    return HttpResponse("<h1>Yo! It's home page</h1>")
+    return render(request, "pages/home.html", context={}, status=200)
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweets_list
+    }
+
+    return JsonResponse(data=data)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    return HttpResponse(f"This is tweet # {tweet_id}")
+    tweet = Tweet.objects.get(id=tweet_id)
+    data = {
+        "id": tweet.id,
+        "content": tweet.content,
+    }
+    return JsonResponse(data=data)
