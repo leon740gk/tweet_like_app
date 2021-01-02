1a47efdb71d37a416a26fa5741cbe81cfa5a400d
leon740gk <leon740gk@gmail.com>
2021-01-02 15:21:54 +0200

    Add user and FK to Tweet model

--- tweets/models.py
+++ tweets/models.py
@@ -1,14 +1,19 @@
 from random import randint
-
+from django.conf import settings
 from django.db import models
 
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="image/", blank=True, null=True)
 
     class Meta:
         ordering = ["-id"]
+
+    def __str__(self):
+        return self.content
 
     def serialize(self):
         return {
--- tweets/views.py
+++ tweets/views.py
@@ -15,10 +15,16 @@ def home_page(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
+
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next")
     if form.is_valid():
         obj = form.save(commit=False)
+        obj.user = request.user or None
         obj.save()
         if request.is_ajax():
             return JsonResponse(obj.serialize(), status=201)
