127ceee79521a4bf2ddba452e6acbb373dbd455b
leon740gk <leon740gk@gmail.com>
2021-01-02 13:24:20 +0200

    Add more func to create tweet form

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,18 @@
+from random import randint
+
 from django.db import models
 
 
 class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to="image/", blank=True, null=True)
+
+    class Meta:
+        ordering = ["-id"]
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": randint(0, 740)
+        }
