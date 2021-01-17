76b31cc025046da75661e1011ca6afaecb1cfdfc
leon740gk <leon740gk@gmail.com>
2021-01-17 16:38:22 +0200

    Make React App powered by Django static

--- tweet_me/urls.py
+++ tweet_me/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from tweets.views import (
     home_page,
@@ -11,8 +14,12 @@ from tweets.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home_page),
+    path("react/", TemplateView.as_view(template_name="react.html")),
     path("tweets", tweet_list_view),
     path("create-tweet", tweet_create_view),
     path("tweets/<int:tweet_id>", tweet_detail_view),
     path("api/tweets/", include("tweets.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
